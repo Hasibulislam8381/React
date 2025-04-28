af52330f1d9c5eac53c417f3f8ada27c8d04fc23
Himel <hasibulislam8381@gmail.com>
2025-04-29 00:09:48 +0600

    Event pass props

--- events/src/CrazyButton.jsx
+++ events/src/CrazyButton.jsx
@@ -1,7 +1,30 @@
-import React from "react";
+import PropTypes from "prop-types";
+function Button({ message, children }) {
+  function handleClick(message) {
+    console.log(message);
+  }
+  return (
+    <button
+      onClick={() => handleClick(message)}
+      className="bg-purple-700 text-white rounded px-2 py-1 cursor-pointer"
+    >
+      {children}
+    </button>
+  );
+}
 
 function CrazyButton() {
-  return <div>CrazyButton</div>;
+  return (
+    <div className="flex gap-1.5">
+      <Button message="dancing back">Dance</Button>
+      <Button message="jumping back">Jump</Button>
+    </div>
+  );
 }
+
+Button.propTypes = {
+  message: PropTypes.string.isRequired,
+  children: PropTypes.node.isRequired,
+};
 
 export default CrazyButton;
