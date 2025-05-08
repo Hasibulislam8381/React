79b0ef8766aca0660103b01cd20c4db2e50e3796
Himel <hasibulislam8381@gmail.com>
2025-05-08 22:19:16 +0600

    cdscs

--- movie-watch/src/MovieList.jsx
+++ movie-watch/src/MovieList.jsx
@@ -1,7 +1,34 @@
+import PropTypes from "prop-types";
 import React from "react";
 
-function MovieList() {
-  return <div>MovieList</div>;
+function MovieList({ movies, rateMovie, toggleWatched, deleteMovie }) {
+  return (
+    <ul>
+      {movies.map((movie) => (
+        <MovieItem
+          key={movie.id}
+          movie={movie}
+          rateMovie={rateMovie}
+          toggleWatched={toggleWatched}
+          deleteMovie={deleteMovie}
+        />
+      ))}
+    </ul>
+  );
 }
 
+MovieList.propTypes = {
+  movies: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number.isRequired,
+      title: PropTypes.string.isRequired,
+      ott: PropTypes.string.isRequired,
+      watched: PropTypes.bool.isRequired,
+      rating: PropTypes.number,
+    })
+  ).isRequired,
+  rateMovie: PropTypes.func.isRequired,
+  toggleWatched: PropTypes.func.isRequired,
+  deleteMovie: PropTypes.func.isRequired,
+};
 export default MovieList;
