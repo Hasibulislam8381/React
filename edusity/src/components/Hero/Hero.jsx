1c1283b2e116d393c1f5dd909ac02b042e05352a
Himel <hasibulislam8381@gmail.com>
2025-05-18 00:41:22 +0600

    cdd

--- edusity/src/components/Hero/Hero.jsx
+++ edusity/src/components/Hero/Hero.jsx
@@ -1,8 +1,22 @@
 import "./Hero.css";
 import React from "react";
+import dark_arrow from "../../assets/dark-arrow.png";
 
 function Hero() {
-  return <div className="hero"></div>;
+  return (
+    <div className="hero container">
+      <div className="hero-text">
+        <h1>We ensure for a better world</h1>
+        <p>
+          Empowering communities through innovation, sustainability, and
+          compassion.
+        </p>
+        <button className="btn">
+          Explore more <img src={dark_arrow} alt="" />
+        </button>
+      </div>
+    </div>
+  );
 }
 
 export default Hero;
