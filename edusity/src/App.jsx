f8903ab14b1046e03dff758ca4aba257e81b3e8f
Himel <hasibulislam8381@gmail.com>
2025-06-03 23:15:24 +0600

    Testimonial section

--- edusity/src/App.jsx
+++ edusity/src/App.jsx
@@ -3,6 +3,7 @@ import Gallery from "./components/Gallery/Gallery";
 import Hero from "./components/Hero/hero";
 import Navbar from "./components/navbar/Navbar";
 import Programs from "./components/Programs/Programs";
+import Testimonial from "./components/Testimonial/Testimonial";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <Programs />
       <About />
       <Gallery />
+      <Testimonial />
     </div>
   );
 }
---
+++ edusity/src/components/Testimonial/Testimonial.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Title from "../Title/Title";
+
+const Testimonial = () => {
+  return (
+    <div>
+      <div className="container mx-auto px-4 pt-14 pb-14">
+        <Title subtitle="Testimonial" title="What Student Says" />
+      </div>
+    </div>
+  );
+};
+
+export default Testimonial;
